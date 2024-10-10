e5ffa0aa1dac2bf263ecfa57ae9a921a9cf98789
Farid Mahmud <47515775+FaridMahmud@users.noreply.github.com>
2024-10-10 18:36:04 +0600

    Update and rename farid.m3u to test.php

---
+++ test.php
@@ -0,0 +1,26 @@
+[
+  {
+    "id": "Live02"
+    "name": "T Sports Live 02",
+    "link": "https://live-cdn.tsports.com/live-02/index.m3u8",
+    "logo": "https://image.tsports.com/images/mobile_logo/1719672720-LIVE.jpg",
+    "origin": "https://live-cdn.tsports.com",
+    "referrer": "https://live-cdn.tsports.com",
+    "userAgent": "Tsports (Linux; Telegram:https://t.me/J_9X_H_9X_N) Github:https://github.com/byte-capsule AndroidXMedia3/1.1.1/64103898/4d2ec9b8c7534adc",
+    "cookie": "Edge-Cache-Cookie=URLPrefix=aHR0cHM6Ly9saXZlLWNkbi50c3BvcnRzLmNvbS8:Expires=1728596112:KeyName=tsports-ed25519-01:Signature=JIhNkE2tupmi3KcW1LMAPzp0QBcRcGLi-B4liiJpy0Vxk2zt4q-skkjcmv06C3oWm_RPZM2hXWrixqg9kpJ4Bw",
+    "drmScheme": "",
+    "drmLicense": ""
+  },
+  {
+    "id": "Live01"
+    "name": "T Sports Live 01",
+    "link": "https://live-cdn.tsports.com/live-01/index.m3u8",
+    "logo": "https://image.tsports.com/images/mobile_logo/1719127161-LIVE.jpg",
+    "origin": "https://live-cdn.tsports.com",
+    "referrer": "https://live-cdn.tsports.com",
+    "userAgent": "Tsports (Linux; Telegram:https://t.me/J_9X_H_9X_N) Github:https://github.com/byte-capsule AndroidXMedia3/1.1.1/64103898/4d2ec9b8c7534adc",
+    "cookie": "Edge-Cache-Cookie=URLPrefix=aHR0cHM6Ly9saXZlLWNkbi50c3BvcnRzLmNvbS8:Expires=1728596112:KeyName=tsports-ed25519-01:Signature=JIhNkE2tupmi3KcW1LMAPzp0QBcRcGLi-B4liiJpy0Vxk2zt4q-skkjcmv06C3oWm_RPZM2hXWrixqg9kpJ4Bw",
+    "drmScheme": "",
+    "drmLicense": ""
+  }
+]
